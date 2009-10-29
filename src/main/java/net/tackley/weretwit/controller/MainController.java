abffb92660cdf1bedf21fd6fdd73d86a26c8f183
Daniel Vydra <dvydra@23359.int.gnl>
2009-10-29 15:54:27 +0000

    cleaned up main controller

--- src/main/java/net/tackley/weretwit/controller/MainController.java
+++ src/main/java/net/tackley/weretwit/controller/MainController.java
@@ -67,39 +67,4 @@ public class MainController extends VelocityViewServlet {
         context.put("tweets", tweets);
     }
 
-
-    protected Template xxx_handleRequest(HttpServletRequest request, HttpServletResponse response, Context context) throws java.lang.Exception {
-        Template tpl = super.getTemplate("test.vm");
-
-        String[] pathBits = request.getRequestURI().split("/");
-        String username = pathBits[pathBits.length - 1];
-        ArrayList tweets = new ArrayList();
-
-        try {
-            List<Status> timeline = twit.getUserTimeline(username);
-            for (Status status : timeline) {
-                String text = status.getText();
-                Date createdAt = status.getCreatedAt();
-                double phaseOfMoon = moon.getPhaseOfMoon(createdAt);
-                String phaseInHex = Integer.toHexString((int) (255 * phaseOfMoon));
-                tweets.add(new TweetVO(text, createdAt, phaseInHex));
-            }
-
-        } catch (TwitterException e) {
-            throw new ServletException(e);
-        }
-
-
-        context.put("tweets", tweets);
-        StringWriter writer = new StringWriter();
-        tpl.merge(context, writer);
-        return tpl;
-    }
-
-    //@Override
-    public void doGetz(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-
-    }
-
 }
