c35ea63fa4018c9814bdb41c34354ca2baae4f63
Daniel Vydra <dvydra@23359.int.gnl>
2009-10-29 14:30:54 +0000

    hackily using vm, but using the proper handle method now.

--- src/main/java/net/tackley/weretwit/controller/MainController.java
+++ src/main/java/net/tackley/weretwit/controller/MainController.java
@@ -8,6 +8,7 @@ import net.tackley.weretwit.utils.PropertiesModule;
 import net.tackley.weretwit.utils.PropertiesProvider;
 import org.apache.velocity.Template;
 import org.apache.velocity.context.Context;
+import org.apache.velocity.tools.view.VelocityView;
 import org.apache.velocity.tools.view.VelocityViewServlet;
 import twitter4j.Status;
 import twitter4j.Twitter;
@@ -25,19 +26,49 @@ import java.util.List;
 @Singleton
 public class MainController extends VelocityViewServlet {
     private Moon moon;
-    private PropertiesProvider propertiesProvider;
+    //private PropertiesProvider propertiesProvider;
     private Twitter twit;
 
     @Inject
     public MainController() {
-        Injector i = Guice.createInjector(Stage.DEVELOPMENT, new MyMainModule(), new PropertiesModule());
+        Injector i = Guice.createInjector(Stage.DEVELOPMENT, new MyMainModule());
         this.moon = i.getInstance(Moon.class);
         this.propertiesProvider = i.getInstance(PropertiesProvider.class);
         this.twit = new Twitter(propertiesProvider.getUsername(), propertiesProvider.getPassword());
+
     }
 
     @Override
-    protected Template handleRequest(HttpServletRequest request, HttpServletResponse response, Context context) throws java.lang.Exception {
+    protected Template getTemplate(HttpServletRequest request, HttpServletResponse response) {
+        return super.getTemplate("test.vm");
+    }
+
+    @Override
+    protected void fillContext(Context context, HttpServletRequest request) {
+        //Template tpl = super.getTemplate("test.vm");
+
+        String[] pathBits = request.getRequestURI().split("/");
+        String username = pathBits[pathBits.length - 1];
+        ArrayList tweets = new ArrayList();
+
+        try {
+            List<Status> timeline = twit.getUserTimeline(username);
+            for (Status status : timeline) {
+                String text = status.getText();
+                Date createdAt = status.getCreatedAt();
+                double phaseOfMoon = moon.getPhaseOfMoon(createdAt);
+                String phaseInHex = Integer.toHexString((int) (255 * phaseOfMoon));
+                tweets.add(new TweetVO(text, createdAt, phaseInHex));
+            }
+        } catch (TwitterException e) {
+            context.put("user_errors", e.getMessage());
+        }
+
+        context.put("tweets", tweets);
+    }
+
+
+    protected Template xxx_handleRequest(HttpServletRequest request, HttpServletResponse response, Context context) throws java.lang.Exception {
         Template tpl = super.getTemplate("test.vm");
 
         String[] pathBits = request.getRequestURI().split("/");
